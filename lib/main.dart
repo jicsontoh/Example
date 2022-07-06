64fef2e98966feb0cf9daa39d943723753b64871
Jicson Toh <jicson.toh@v-key.com>
2022-07-06 17:31:29 +0700

    Change test1

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Test',
+              'Test1',
             ),
             Text(
               '$_counter',
